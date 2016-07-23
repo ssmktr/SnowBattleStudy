a1a8158bcf49614900717461c04eb78e3c4efd4c
Juhong Baek <white2wide@naver.com>
2016-07-23 19:51:30 +0900

    no message

--- BJH/Assets/3.Scripts/Player.cs
+++ BJH/Assets/3.Scripts/Player.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     private Rigidbody Rigidbody;
+	private Animator anim;
 
 	// Use this for initialization
 	void Awake () {
 
 
         Rigidbody = this.GetComponent<Rigidbody>();
-
+		anim = this.transform.FindChild("Robot Kyle").GetComponent<Animator> ();
 
     }
 	
@@ -18,12 +19,38 @@ public class Player : MonoBehaviour {
 	void Update () {
 
 
-        if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKey(KeyCode.W))
         {
 
-            Rigidbody.AddForce(new Vector3(0, 0, 100));
+            //Rigidbody.AddForce(new Vector3(0, 0, 100));
+			Rigidbody.velocity = new Vector3(0,0,5);
+
+			 
 
         }
 
+		if (Input.GetKey(KeyCode.S))
+		{
+
+			//Rigidbody.AddForce(new Vector3(0, 0, 100));
+			Rigidbody.velocity = new Vector3(0,0,-5);
+
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+
+			//Rigidbody.AddForce(new Vector3(0, 0, 100));
+			Rigidbody.velocity = new Vector3(5,0,0);
+
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+
+			//Rigidbody.AddForce(new Vector3(0, 0, 100));
+			Rigidbody.velocity = new Vector3(-5,0,0);
+
+		}
 	}
 }
