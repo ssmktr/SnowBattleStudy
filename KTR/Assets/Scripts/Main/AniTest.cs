6824cda170a4ea6792cee48cd45a9f90a3aee527
unknown <>
2016-07-24 22:42:55 +0900

    베이스 커밋

---
+++ KTR/Assets/Scripts/Main/AniTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AniTest : MonoBehaviour {
+    public Animator Anim;
+    public Rigidbody MyRigidbody;
+    float h, v;
+
+    void Start () {
+        
+    }
+
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            Anim.Play("Idle");
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            Anim.Play("Walk_Forward");
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            Anim.Play("Walk_Right");
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            Anim.Play("Walk_Back");
+        }
+    }
+}
---
+++ KTR/Assets/Scripts/UnitBase.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitBase : MonoBehaviour {
+
+    public enum AniType
+    {
+        Idle = 0,
+
+        Walk_Forward,
+        Walk_Back,
+        Walk_Right,
+        Walk_Left,
+
+        Run_Forward,
+        Run_Back,
+    };
+
+    public Animator Anim;
+
+    Dictionary<AniType, string> DicAnimation = new Dictionary<AniType, string>();
+
+	void Start () {
+        DicAnimation.Add(AniType.Idle, "Idle");
+        DicAnimation.Add(AniType.Walk_Forward, "Walk_Forward");
+        DicAnimation.Add(AniType.Walk_Back, "Walk_Back");
+        DicAnimation.Add(AniType.Walk_Right, "Walk_Right");
+        DicAnimation.Add(AniType.Walk_Left, "Walk_Left");
+        DicAnimation.Add(AniType.Run_Forward, "Run_Forward");
+        DicAnimation.Add(AniType.Run_Back, "Run_Back");
+    }
+
+    void Update()
+    {
+        ControlAnimation();
+    }
+
+    public virtual void SetAnimation(AniType eType)
+    {
+        if (Anim != null)
+            Anim.Play(DicAnimation[eType]);
+    }
+
+    public virtual void ControlAnimation()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            SetAnimation(AniType.Run_Forward);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
+        {
+            SetAnimation(AniType.Run_Back);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            SetAnimation(AniType.Walk_Forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            SetAnimation(AniType.Walk_Back);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            SetAnimation(AniType.Walk_Right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            SetAnimation(AniType.Walk_Left);
+        }
+        else
+        {
+            SetAnimation(AniType.Idle);
+        }
+    }
+}
